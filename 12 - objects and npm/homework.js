b06e47ad2d66ddb0d0d55aac276adb955eddca3c
Igor_Nazarov <nazarov.igor.anatolievich@gmal.com>
2023-12-17 11:14:23 +0200

    Homework9

--- 12 - objects and npm/homework.js
+++ 12 - objects and npm/homework.js
@@ -9,6 +9,14 @@
 3. Виведіть об'єкт student в консоль. 
 */
 
+let student = {};
+    student.firstName = 'Ivan';
+    student.lastName = 'Ivanov';
+    student.age = 23;
+    student.courses = ['Math', 'History', 'Programming'];
+
+console.log(student);
+
 
 /* 
 Завдання 2: Доступ до властивостей об'єкта
@@ -18,7 +26,13 @@
     - year: число, представляє рік видання книги.
 2. Виведіть в консоль назву та автора книги з об'єкта book. 
 */
+let book = {
+    title: 'Idiot',
+    author: 'Dostoevskiy',
+    year: 1869
+};
 
+console.log(`\n The book title is ${book.title}, \nthe author is ${book.author}`)
 
 /* 
 Завдання 3: Зміна властивостей об'єкта
@@ -29,7 +43,15 @@
     - Змініть рік випуску автомобіля на новий рік.
 2. Виведіть оновлений об'єкт car в консоль. 
 */
+let car = {
+    brand: 'Audi',
+    model: 'TT',
+    year: 2022,
+};
 
+car.year = 2023;
+console.log(`\n`)
+console.log(car);
 
 /* 
 Завдання 4: Додавання та видалення властивостей
@@ -42,6 +64,16 @@
 4. Виведіть оновлений об'єкт person в консоль.
 */
 
+let person = {
+    firstName: 'Ivan',
+    lastName: 'Ivanov',
+    age: 23
+};
+
+person.email = 'test@test.com';
+delete person.age;
+console.log(`\n`)
+console.log(person)
 
 /*
 Завдання 5: Перебір властивостей об'єкта
@@ -49,3 +81,30 @@
 2. Використовуючи цикл for...of, переберіть всі елементи масиву та виведіть їхні значення в консоль.
 3. Зробіть деструктуризацію в циклі
 */
+
+let users = [
+    {
+        firstName: 'Ivan',
+        lastName: 'Ivanov',
+        age: 23
+    },
+    {
+        firstName: 'Petr',
+        lastName: 'Petrov',
+        age: 24
+    },
+    {
+        firstName: 'Sidor',
+        lastName: 'Sidorov',
+        age: 25
+    },
+];
+
+console.log(`\n`)
+for (let user of users) {
+    let {firstName, lastName, age} = user;
+    console.log(firstName);
+    console.log(lastName);
+    console.log(age);
+    console.log(`\n`);
+};
